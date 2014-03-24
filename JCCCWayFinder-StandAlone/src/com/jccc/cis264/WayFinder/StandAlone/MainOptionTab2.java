aecf68c7d79fe3f0b279f69754595a826721d594
Andrew Tribble <Andrewtr001@gmail.com>
2014-03-24 14:59:22 -0500

    Update_047A1
    Updated formatting of some of the added code in Update_047A

--- JCCCWayFinder-StandAlone/src/com/jccc/cis264/WayFinder/StandAlone/MainOptionTab2.java
+++ JCCCWayFinder-StandAlone/src/com/jccc/cis264/WayFinder/StandAlone/MainOptionTab2.java
@@ -2,6 +2,8 @@ package com.jccc.cis264.WayFinder.StandAlone;
 
 import java.util.ArrayList;
 
+import com.google.android.gms.maps.model.LatLng;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.ProgressDialog;
@@ -28,6 +30,7 @@ public class MainOptionTab2 extends Activity {
 		final ArrayList<String> Destinations = new ArrayList();
 		final Intent startMap = new Intent(this, MainOptions.class);
 		final ArrayList<String> valueArray = new ArrayList();
+		final LatLng CC = new LatLng(38.9252624, -94.7279145), LIB = new LatLng(38.924214, -94.72767190000002), RC = new LatLng(38.9243700, -94.72684989999999), OCB = new LatLng(38.9245613,-94.728429), CLB = new LatLng(38.9233935, -94.7278336), SCI = new LatLng(38.9237222, -94.72876329999997), HCA = new LatLng(38.9230872,-94.72655580000003), GP = new LatLng(38.9233574, -94.7292281), SC = new LatLng(38.9245847, -94.73058229999998), CSB = new LatLng(38.9231457, -94.72993550000001), ATB = new LatLng(38.9231911,-94.7308248), WLB = new LatLng(38.9221687, -94.73094609999998), GYM = new LatLng(38.9240505, -94.73171409999998), GEB = new LatLng(38.924214, -94.7292281), COM = new LatLng(38.9245613,-94.72993550000001), HSC = new LatLng(38.9237438, -94.73527139999999), ITC = new LatLng(38.9225006, -94.73179490000001), PA = new LatLng(38.924395, -94.73527139999999);
 
 		valueArray.add("ATB"); valueArray.add("LIB"); valueArray.add("CC"); valueArray.add("CSB"); valueArray.add("CLB"); valueArray.add("GP"); valueArray.add("GEB"); valueArray.add("GYM"); valueArray.add("HSC"); valueArray.add("HCA"); valueArray.add("ITC"); valueArray.add("OCB"); valueArray.add("PA"); valueArray.add("RC"); valueArray.add("SCI"); valueArray.add("SC"); valueArray.add("WLB");
 
@@ -36,17 +39,17 @@ public class MainOptionTab2 extends Activity {
 			public void onClick(View v) {
 				Destinations.clear();
 
-				Destinations.add(valueArray.get(destSpin.getSelectedItemPosition()));
+				valueArray.get(destSpin.getSelectedItemPosition());
 				System.out.println(Destinations);
-				startMap.removeExtra("Destination Array");
 				startMap.putStringArrayListExtra("Destination Array", Destinations);
+
 				//myActivity.startActivity(startMap);
 			}
 		});
 	}
 
 
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
