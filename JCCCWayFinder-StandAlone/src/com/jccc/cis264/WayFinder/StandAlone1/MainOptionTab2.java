ba67631d7a901a838deeb25b5edb72213cc66deb
Andrew Tribble <Andrewtr001@gmail.com>
2014-05-06 15:32:27 -0500

    JCCC WayFinder 1.0 Update1
    Fixes an issue where some buildings could be drawn in the wrong place
    when going from building to building in “By Building” mode

---
+++ JCCCWayFinder-StandAlone/src/com/jccc/cis264/WayFinder/StandAlone1/MainOptionTab2.java
@@ -0,0 +1,128 @@
+package com.jccc.cis264.WayFinder.StandAlone1;
+
+import java.util.ArrayList;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.jccc.cis264.WayFinder.StandAlone.R;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+
+@SuppressWarnings({ "all" })
+public class MainOptionTab2 extends Activity {
+
+	private Activity myActivity = this;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_option_tab2);
+
+		final Spinner destSpin = (Spinner) findViewById(R.id.spinner2);
+		final Spinner FromSpin = (Spinner) findViewById(R.id.spinner1);
+		final ArrayList<String> Destinations = new ArrayList();
+		final Intent startMap;
+		final ArrayList<String> valueArray = new ArrayList();
+
+		final Button GoButton1A = (Button) findViewById(R.id.GoButton2A);
+		GoButton1A.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Destinations.clear();
+				if(FromSpin.getSelectedItemPosition() == 0){
+					Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse("google.navigation:q=" + findLatLngByCode(destSpin.getSelectedItemPosition(), 0).latitude + "," + findLatLngByCode(destSpin.getSelectedItemPosition(), 0).longitude + "&mode=w"));
+					myActivity.startActivity(intent);
+				}
+				else{
+					Intent intent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse("http://maps.google.com/maps?saddr=" + findLatLngByCode(FromSpin.getSelectedItemPosition(), 1).latitude + "," + findLatLngByCode(FromSpin.getSelectedItemPosition(), 1).longitude + "&daddr=" + findLatLngByCode(destSpin.getSelectedItemPosition(), 0).latitude + "," + findLatLngByCode(destSpin.getSelectedItemPosition(), 0).longitude + "&mode=w"));
+					intent.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
+					myActivity.startActivity(intent);
+				}
+			}
+		});
+	}
+
+
+	private LatLng findLatLngByCode(int code, int offset){
+		
+		int use = code - offset;
+		
+		LatLng CC = new LatLng(38.9252624, -94.7279145),
+				LIB = new LatLng(38.924214, -94.72767190000002),
+				RC = new LatLng(38.9243700, -94.72684989999999),
+				OCB = new LatLng(38.9245613,-94.728429),
+				CLB = new LatLng(38.9233935, -94.7278336),
+				SCI = new LatLng(38.9237222, -94.72876329999997),
+				HCA = new LatLng(38.9230872,-94.72655580000003),
+				GP = new LatLng(38.9233574, -94.7292281),
+				SC = new LatLng(38.9245847, -94.73058229999998),
+				CSB = new LatLng(38.9231457, -94.72993550000001),
+				ATB = new LatLng(38.9231911,-94.7308248),
+				WLB = new LatLng(38.9221687, -94.73094609999998),
+				GYM = new LatLng(38.9240505, -94.73171409999998),
+				GEB = new LatLng(38.924214, -94.7292281),
+				COM = new LatLng(38.9245613,-94.72993550000001),
+				HSC = new LatLng(38.9237438, -94.73527139999999),
+				ITC = new LatLng(38.9225006, -94.73179490000001),
+				PA = new LatLng(38.924395, -94.73527139999999);
+
+		if(use == 1)
+			return LIB;
+		else if(use == 2)
+			return CC;
+		else if(use == 14)
+			return RC;
+		else if(use == 12)
+			return OCB;
+		else if(use == 4)
+			return CLB;
+		else if(use == 10)
+			return HCA;
+		else if(use == 6)
+			return GP;
+		else if(use == 16)
+			return SC;
+		else if(use == 3)
+			return CSB;
+		else if(use == 0)
+			return ATB;
+		else if(use == 17)
+			return WLB;
+		else if(use == 8)
+			return GYM;
+		else if(use == 7)
+			return GEB;
+		else if(use == 5)
+			return COM;
+		else if(use == 9)
+			return HSC;
+		else if(use == 11)
+			return ITC;
+		else if(use == 13)
+			return PA;
+		else if(use == 15)
+			return SCI;
+		else{
+			return null;
+		}
+	
+}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_option_tab2, menu);
+		return true;
+	}
+
+}
---
+++ JCCCWayFinder-StandAlone/src/com/jccc/cis264/WayFinder/StandAlone1/Section.java
@@ -0,0 +1,218 @@
+package com.jccc.cis264.WayFinder.StandAlone1;
+
+import java.util.ArrayList;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Represents a Student's individual courses&#046; Each instance of Section will be uniquely initialized based on that semister's catalog&#046;</br><i>Created by: Term</i>
+ * 
+ */
+@SuppressWarnings("all")
+public class Section{
+
+	private String SectionId, SectionTitle, CourseName, CourseDescription, CourseSectionNumber, FirstMeetingDate, LastMeetingDate, Ceus, LearningProvider, LearningProviderSiteId, PrimarySectionId;
+	private int Credits, SectionNumber;
+	@SuppressWarnings("unused")
+	private Boolean IsInstructor, isF2F;
+	private MeetingPattern MeetingPattern;
+	private ArrayList<Instructor> Instructor = new ArrayList();
+
+	/**
+	 * Creates a fully initialized instance of Section.  Also checks to see if a section is online or face-to-face by counting the number of F2F meeting patterns.
+	 * @param data JSON data containing section information
+	 * @throws JSONException if JSON input data is blank or unreadable
+	 * @see MeetingPattern
+	 * @See Instructor
+	 * @See 
+	 */
+	public Section(JSONObject data) throws JSONException{
+		SectionId = data.getString("sectionId");
+		SectionTitle = data.getString("sectionTitle");
+		CourseName = data.getString("courseName");
+		CourseDescription = data.getString("courseDescription");
+		CourseSectionNumber = data.getString("courseSectionNumber");
+		FirstMeetingDate = data.getString("firstMeetingDate");
+		LastMeetingDate = data.getString("lastMeetingDate");
+		Ceus = data.getString("ceus");
+		LearningProvider = data.getString("learningProvider");
+		LearningProviderSiteId = data.getString("learningProviderSiteId");
+		PrimarySectionId = data.getString("primarySectionId");
+		Credits = data.getInt("credits");
+		IsInstructor = data.getBoolean("isInstructor");
+
+		if(data.getJSONArray("meetingPatterns").length() > 0){
+			MeetingPattern = new MeetingPattern(data.getJSONArray("meetingPatterns").getJSONObject(0));
+		}else{
+			MeetingPattern = null;
+		}
+
+		if(data.getJSONArray("instructors").length() > 0){
+			for(int i = 0; i < data.getJSONArray("instructors").length(); i++){
+				if(data.getJSONArray("instructors").getJSONObject(i).length() > 0){
+					Instructor.add(new Instructor(data.getJSONArray("instructors").getJSONObject(i)));
+				}
+				else{
+
+				}
+			}
+		}
+
+		if(data.getJSONArray("meetingPatterns").length() == 0){
+			isF2F = false;
+		}
+		else{
+			isF2F = true;
+		}
+
+	}
+
+	
+	/**
+	 * gets the section number as listed in the interface. Section numbers are dynamic, and assigned at runtime by {@link #setSectionNumber(int)}
+	 * @return the interface section order
+	 */
+	public int getSectionNumber() {
+		return SectionNumber;
+	}
+
+	/**
+	 * Sets the section number of the current instance&#046 Dynamically called at runtime&#046
+	 * @param sectionNumber sets the Section number
+	 */
+	public void setSectionNumber(int sectionNumber) {
+		SectionNumber = sectionNumber;
+	}
+
+	/**
+	 * gets the CRN Section ID of the course represented by the current instance.
+	 * @return the section ID
+	 */
+	public String getSectionId() {
+		return SectionId;
+	}
+
+	/**
+	 * gets the title of the course represented by the current instance
+	 * @return the course's section title
+	 */
+	public String getSectionTitle() {
+		return SectionTitle;
+	}
+
+	/**
+	 * gets the name of the course represented by the current instance.
+	 * @return the course name
+	 */
+	public String getCourseName() {
+		return CourseName;
+	}
+
+	/**
+	 * gets the description of the course represented by the current instance.
+	 * @return the course description
+	 */
+	public String getCourseDescription() {
+		return CourseDescription;
+	}
+
+	/**
+	 * gets the campus assigned course section number of the course represented by the current instance.
+	 * @return campus section number
+	 */
+	public String getCourseSectionNumber() {
+		return CourseSectionNumber;
+	}
+
+	/**
+	 * gets the date of the first class session of the course represented by the current instance.
+	 * @return the first meeting date as a String
+	 */
+	public String getFirstMeetingDate() {
+		return FirstMeetingDate;
+	}
+
+	/**
+	 * gets the date of the last class session of the course represented by the current instance.
+	 * @return the last meeting date as a String
+	 */
+	public String getLastMeetingDate() {
+		return LastMeetingDate;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public String getCeus() {
+		return Ceus;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public String getLearningProvider() {
+		return LearningProvider;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public String getLearningProviderSideId() {
+		return LearningProviderSiteId;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public String getPrimarySectionId() {
+		return PrimarySectionId;
+	}
+
+	/**
+	 * gets the total credit hours for the course, as defined by the campus.
+	 * @return course credit hours
+	 */
+	public int getCredits() {
+		return Credits;
+	}
+
+	/**
+	 * gets whether or not the current data is infact an instructor, rather than a student
+	 * @return returns whether the student is an instructor as Boolean
+	 */
+	public Boolean isInstructor() {
+		return IsInstructor;
+	}
+
+	/**
+	 * gets whether or not the current section is an online course.
+	 * @return Boolean whether the course is online
+	 */
+	public Boolean isOnline() {
+		return !isF2F;
+	}
+
+	/**
+	 * returns the primary meeting pattern of the course represented by the current instance
+	 * @return MeetingPattern 0 from JSON input data
+	 * @see MeetingPattern
+	 */
+	public MeetingPattern getMeetingPattern() {
+		return MeetingPattern;
+	}
+
+	/**
+	 * returns the instructor specified by the index (primary instructor is 0)
+	 * @param index - the index of the instructor you wish to view
+	 * @return the Instructor specified by the index
+	 * @see Instructor
+	 */
+	public Instructor getInstructor( int index) {
+		return Instructor.get(index);
+	}
+
+}
