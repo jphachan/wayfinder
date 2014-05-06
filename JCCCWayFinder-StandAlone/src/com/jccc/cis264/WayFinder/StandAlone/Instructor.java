368013092218f323da60b37b78b3c690a7238371
Andrew Tribble <Andrewtr001@gmail.com>
2014-05-05 22:46:41 -0500

    Fixed issue with map types drawing incorrectly

---
+++ JCCCWayFinder-StandAlone/src/com/jccc/cis264/WayFinder/StandAlone/Instructor.java
@@ -0,0 +1,75 @@
+package com.jccc.cis264.WayFinder.StandAlone;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Contains information about the Instructor of the course calling the constructor&#046;</br><i>Created by: Section</i>
+ * 
+ */
+public class Instructor {
+	private String FirstName, LastName, MiddleInitial, InstructorId, Primary, FormattedName;
+	
+	/**
+	 * Creates a fully initialized instance of Instructor when calle</br>Created by: Section
+	 * @param data the incoming JSON data
+	 * @throws JSONException Thrown if the JSON data is empty, or unreadable
+	 */
+	public Instructor(JSONObject data) throws JSONException{
+		FirstName = data.getString("firstName");
+		LastName = data.getString("lastName");
+		MiddleInitial = data.getString("middleInitial");
+		InstructorId = data.getString("instructorId");
+		Primary = data.getString("primary");
+		FormattedName = data.getString("formattedName");
+	}
+	
+	/**
+	 * gets the First Name of the current instructor
+	 * @return the FIRST NAME of the instructor
+	 */
+	public String getFirstName() {
+		return FirstName;
+	}
+
+	/**
+	 * gets the Last Name of the current instructor
+	 * @return the LAST NAME of the instructor
+	 */
+	public String getLastName() {
+		return LastName;
+	}
+
+	/**
+	 * gets the Middle Initial, or in most cases the middle name, of the current instructor
+	 * @return the MIDDLE INITIAL of the instructor
+	 */
+	public String getMiddleInitial() {
+		return MiddleInitial;
+	}
+
+	/**
+	 * gets the Instructor ID of the current instructor
+	 * @return the Instructor id of the instructor
+	 */
+	public String getInstructorId() {
+		return InstructorId;
+	}
+
+	/**
+	 * returns whether the current instructor is the primary instructor.
+	 * @return whether the the instructor is primary
+	 */
+	public String getPrimary() {
+		return Primary;
+	}
+
+	/**
+	 * gets the formatted string containing the instructors full name 
+	 * @return  the formatted name of type strong
+	 */
+	public String getFormattedName() {
+		return FormattedName;
+	}
+
+}
---
+++ JCCCWayFinder-StandAlone/src/com/jccc/cis264/WayFinder/StandAlone/Term.java
@@ -0,0 +1,92 @@
+package com.jccc.cis264.WayFinder.StandAlone;
+
+import java.util.ArrayList;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+/**
+ * Contains information about a particular term&#046; </br><i>Created by: Student</i>
+ * @see Student
+ */
+@SuppressWarnings("all")
+public class Term {
+
+	private String id, name, startDate, endDate;
+	ArrayList<Section> section = new ArrayList();
+	
+	/**
+	 * Creates a fully initialized instance of Instructor when called.</br>Also triggers multiple instances of Section.
+	 * @param data the JSON data provided by the student class, as provided by the Banner Mobile API at runtime.
+	 * @see Student
+	 * @see Section
+	 */
+	public Term(JSONObject data) throws JSONException {
+		id = data.getString("id");
+		name = data.getString("name");
+		startDate = data.getString("startDate");
+		endDate = data.getString("endDate");
+
+		if(data.getJSONArray("sections").length() > 0){
+			for(int i = 0; i < data.getJSONArray("sections").length(); i++){
+				if(data.getJSONArray("sections").getJSONObject(i).length() > 0){
+					section.add(new Section(data.getJSONArray("sections").getJSONObject(i)));
+					section.get(i).setSectionNumber(i);
+				}
+			}
+		}
+	}
+
+
+	/**
+	 * gets the campus wide ID of term represented by the current instance
+	 * @return the respective term's ID 
+	 */
+	public String getId() {
+		return id;
+	}
+	
+	/**
+	 * gets the name  (e.g. Fall 2014) of the term represented by the current instance
+	 * @return the respective term's name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * gets the Start Date of the term represented by the current instance
+	 * @return the respective term's Start Date
+	 */
+	public String getStartDate() {
+		return startDate;
+	}
+
+	/**
+	 * gets the End Date of the term represented by the current instance
+	 * @return the respective term's End Date
+	 */
+	public String getEndDate() {
+		return endDate;
+	}
+
+	/**
+	 * returns all of the student's sections contained in this respective term.
+	 * @return an ArrayList of type Section
+	 * @see Section
+	 */
+	public ArrayList<Section> getSections() {
+		return section;
+	}
+	
+	/**
+	 * returns a specific section, based on the provided index, and the Student's Term
+	 * @param index the index of the Section to be returned
+	 * @return the respective object Section, given the index
+	 * @see Section
+	 */
+	public Section getSection(int index) {
+		return section.get(index);
+	}
+}
